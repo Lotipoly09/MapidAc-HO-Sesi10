214f9de3556a62e2eb131713653a7e55f6772d9e
Loti Polynorth <lotipolynorth@gmail.com>
2026-07-16 12:47:03 +0700

    nambahin titik

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -8,5 +8,46 @@ document.body.appendChild(mapElement);
 const map = new Map({
   container: 'map',
   style: 'https://demotiles.maplibre.org/globe.json',
+  center: [114, 0.2],
+  zoom: 2.5,
 });
 
+const data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "NAMOBJ": "Indonesia"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          112.7415576,
+          -4.2901985
+        ]
+      }
+    }
+  ]
+}
+
+map.on('load', () => {
+map.addSource('indonesia-source', {
+    type: 'geojson',
+    data: data,
+  });
+
+  map.addLayer({
+    id: 'titik-indonesia',
+    type: 'circle',
+    source: 'indonesia-source',
+    paint: {
+      'circle-radius': 5,
+      'circle-color': '#FF0000',
+      'circle-stroke-width': 2,
+      'circle-stroke-color': '#000000',
+    },
+  });
+} )
+  
+  
